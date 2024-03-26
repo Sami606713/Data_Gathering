05b4c65e49c98a03284c8d1ac04aae860ae7e9b8
samiullah <sami606713@gmail.com>
2024-03-26 11:12:18 +0500

    Gathering News Data

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from src.data_gathering.logging import logging
 from src.data_gathering.exception import CustomException
-from src.data_gathering.utils import database_connection,getting_data
+from src.data_gathering.utils import database_connection,getting_data,insert_data,clean_data
 import sys,time
 import streamlit as st
-
 
 def main():
     try:
         st.title("data gathering")
         st.write("conneting to db")
-        # conn=database_connection("International_news",'Headlines')
+        conn=database_connection("International_news",'headlines')
         st.write("conneting successfull")
 
         logging.info("creating the database")
@@ -20,20 +19,26 @@ def main():
         if "data" not in session_state:
             session_state.data = None
 
-        if st.button("Start gathering"):
-            st.write("ok sir")
+        # dic = None
+        if st.button("View Data"):
+            # st.write("ok sir")
             session_state.data=data=getting_data()
-            st.dataframe(session_state.data)
-        if(session_state.data is not None):
-            if(st.button("Export Data")):
-                st.write("exporting")
-                with st.status("Downloading data..."):
-                    st.write("Searching for data...")
-                    time.sleep(2)
-                    st.write("Found URL.")
-                    time.sleep(1)
-                    st.write("Downloading data...")
-                    time.sleep(1)
+            dic=clean_data(session_state.data)
+            st.dataframe(dic)
+            # st.write(dic['title'])
+        if session_state.data is not None and st.button("Export Data"):
+            st.write("exporting")
+            with st.status("Exporting data..."):
+                dic=clean_data(session_state.data)
+                # st.write(dic)
+                insert_data(dic=dic)
+                st.write("Searching for data...")
+                time.sleep(2)
+                # st.write(f"done {dic}")
+                # time.sleep(1)
+                # st.write("Downloading data...")
+                # # insert_data()
+                # time.sleep(1)
     except Exception as e:
         raise CustomException(e,sys)
 
@@ -45,3 +50,4 @@ if __name__=="__main__":
     #     # getting_data()
     # except Exception as e:
     #     raise CustomException(e,sys)
+
--- src/data_gathering/utils.py
+++ src/data_gathering/utils.py
@@ -3,6 +3,8 @@ from src.data_gathering.exception import CustomException
 import pymysql,os,sys
 from dotenv import load_dotenv
 import requests
+import pandas as pd
+import streamlit as st
 
 load_dotenv()
 
@@ -10,7 +12,7 @@ host=os.getenv('host')
 user=os.getenv('user')
 password=os.getenv('password')
 
-def database_connection(database_name,table_name):
+def connection():
     try:
         logging.info("Connecting to database")
 
@@ -19,8 +21,15 @@ def database_connection(database_name,table_name):
             user=user,
             password=password
         )
-        logging.info("connection successfull")
-        return create_db(conn=conn,database_name=database_name,table_name=table_name)
+        logging.info("connection successfully")
+        return conn
+        # return create_db(conn=conn,database_name=database_name,table_name=table_name)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def database_connection(database_name,table_name):
+    try:
+        return create_db(conn=connection(),database_name=database_name,table_name=table_name)
     except Exception as e:
         raise CustomException(e,sys)
 
@@ -41,15 +50,16 @@ def create_db(conn,database_name,table_name):
 
 def create_table(conn,database_name,table_name):
     with conn.cursor() as cursor:
-        query = f"""CREATE TABLE {database_name}.{table_name} (
-            author VARCHAR(50) DEFAULT NULL,
-            title VARCHAR(250) DEFAULT NULL,
-            description TEXT DEFAULT NULL,
-            published_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            content TEXT DEFAULT NULL,
-            url varchar(255) default null,
-            url_to_image varchar(255) default null
-        )"""
+        query = f"""CREATE TABLE if not exists {database_name}.{table_name} (
+                news_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                author VARCHAR(50) DEFAULT NULL,
+                title VARCHAR(250) DEFAULT NULL,
+                description TEXT DEFAULT NULL,
+                published_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                content TEXT DEFAULT NULL,
+                url VARCHAR(255) DEFAULT NULL,
+                url_to_image VARCHAR(255) DEFAULT NULL
+            );"""
 
         cursor.execute(query)
             
@@ -57,10 +67,82 @@ def create_table(conn,database_name,table_name):
         logging.info("database created")
 
 def getting_data():
-    url='https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=be805478ab5840a3b820a974b56fe770'
+    api=os.getenv("api")
+    # st.write(api)
+    url=f'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey={api}'
     response=requests.get(url)
-    print(response.json()['articles'])
-    return response.json()['articles']
+    # print(response.json()['articles'])
+    response=response.json()['articles']
 
-def insert_data(conn,response):
-    pass
+    return response
+    # return insert_data(conn=connection(),response=response)
+
+from datetime import datetime
+
+def insert_data(dic):
+    try:
+        conn = connection()  # Assuming connection() returns a valid connection object
+        with conn.cursor() as cursor:
+            # Iterate over each row in the DataFrame
+            for row in dic.to_dict('records'):
+                # Convert the datetime string to a valid MySQL datetime format
+                published_at = datetime.strptime(row['date'], '%Y-%m-%dT%H:%M:%SZ')
+                
+                query = """INSERT INTO International_news.headlines 
+                           (news_id, author, title, description, published_at, content, url, url_to_image) 
+                           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+                data = (None, row['author'], row['title'], row['desc'], 
+                        published_at, row['content'], row['url'], row['url-to-image'])
+                cursor.execute(query, data)
+        conn.commit()
+        st.write("Successfully inserted data into the database.")
+    except Exception as e:
+        st.write("Error:", e)
+    finally:
+        conn.close()  # Make sure to close the connection after use
+
+def clean_data(response):
+    # st.write("ok")
+    # response=response
+    author=[]
+    title=[]
+    desc=[]
+    content=[]
+    pub_date=[]
+    url=[]
+    url_to_image=[]
+    for i in response:
+        # st.write("clean your data")
+        # st.write(i["author"])
+        author.append(i["author"])
+
+        # st.write(i["title"])
+        title.append(i["title"])
+
+        # st.write(i["description"])
+        desc.append(i["description"])
+
+        # st.write(i["content"])
+        content.append(i["content"])
+
+        # st.write(i["publishedAt"])
+        pub_date.append(i["publishedAt"])
+
+        # st.write(i["url"])
+        url.append(i["url"])
+
+        # st.write(i["urlToImage"])
+        url_to_image.append(i["urlToImage"])
+        # st.write(i)
+        # break
+    dic={
+        'author':author,
+        'title':title,
+        'desc':desc,
+        'content':content,
+        'date':pub_date,
+        'url':url,
+        'url-to-image':url_to_image
+    }
+
+    return pd.DataFrame(dic)
